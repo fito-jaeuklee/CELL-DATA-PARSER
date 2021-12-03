254c0108497fc43816ea4c2ddef0bfca68639120
Jaebok Shin <jaebok.shin@fitogether.com>
2021-12-03 18:04:04 +0900

    use memoryview instead of byte split

--- newcell/intermed_generator.py
+++ newcell/intermed_generator.py
@@ -2,7 +2,7 @@ from newcell.managers.gps_message_manager import GpsMessageManager
 from newcell.managers.imu_message_manager import ImuMessageManager
 from newcell.managers.bs_message_manager import BsMessageManager
 from newcell.messages.start_message import StartMessage
-#from newcell.messages.end_message import EndMessage
+from newcell.messages.end_message import EndMessage
 
 
 HEADER_DELIMITER = b"COACH"
@@ -27,13 +27,6 @@ class IntermedGenerator:
     }
 
     @staticmethod
-    def _strip_off_bytes(message: bytes):
-        data = message[:CRC_IDX]
-        crc = message[CRC_IDX]
-
-        return (data, crc)
-
-    @staticmethod
     def _crc_check(data_to_check, crc_value):
         crc = 0
 
@@ -42,31 +35,57 @@ class IntermedGenerator:
 
         return crc == crc_value
 
+    @staticmethod
+    def raw_parser(file_contents: memoryview):
+        MSG_TYPE_OFFSET = len(HEADER_DELIMITER)
+        MSG_LENGTH_OFFSET = MSG_TYPE_OFFSET + 1
+        MSG_PAYLOAD_OFFSET = MSG_LENGTH_OFFSET + 1
+
+        cursor = 0
+
+        try:
+            while True:
+                msg_first_index = file_contents.obj.index(HEADER_DELIMITER, cursor)
+
+                msg_type_index = msg_first_index + MSG_TYPE_OFFSET
+                msg_length_index = msg_first_index + MSG_LENGTH_OFFSET
+                msg_payload_index = msg_first_index + MSG_PAYLOAD_OFFSET
+
+                msg_length = file_contents[msg_length_index]
+                msg_crc_index = msg_payload_index + msg_length
+
+                msg_type = file_contents[msg_type_index]
+                crc_data = file_contents[msg_type_index:msg_crc_index]
+                payload = file_contents[msg_payload_index:msg_crc_index]
+                crc = file_contents[msg_crc_index]
+
+                yield (msg_type, crc_data, payload, crc)
+
+                cursor = msg_crc_index
+        except Exception:
+            return
+
     @classmethod
     def parse(cls, file_contents):
-        messages = file_contents.split(HEADER_DELIMITER) 
+        mv_contents = memoryview(file_contents)
 
         start_msg = None
 
-        for message in messages:
-            data, crc = IntermedGenerator._strip_off_bytes(message)
-
-            # XXX end message should be followed by "\r\n" ?? -> check!
-            if data[0] != END_MSG_TYPE and not IntermedGenerator._crc_check(data, crc):
+        for (message_type, crc_data, payload, crc) in IntermedGenerator.raw_parser(mv_contents):
+            if not IntermedGenerator._crc_check(crc_data, crc):
                 # do error handling (logging??)
                 continue
-
-            payload = data[PAYLOAD_START_IDX:]
-            message_type = data[0]
 
             if message_type == START_MSG_TYPE:
                 start_msg = StartMessage.create(payload)
             elif message_type == END_MSG_TYPE:
+                end_message = EndMessage.create(payload)
                 yield (
                     start_msg,
                     cls.gps_message_manager.export_dataframe(),
                     cls.imu_message_manager.export_dataframe(),
                     cls.bs_message_manager.export_dataframe(),
+                    end_message,
                 )
             elif message_type in cls.managers:
                 cls.managers[message_type].add_message(payload)
